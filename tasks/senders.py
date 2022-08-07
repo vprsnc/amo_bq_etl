0b35dfedc57a55591dac009870ab4def7ec321be
Georgy Toporkov <83061636+vprsnc@users.noreply.github.com>
2022-08-07 18:16:48 +0300

    debugging

--- tasks/senders.py
+++ tasks/senders.py
@@ -14,9 +14,9 @@ def leads_sender():
 
     start = time.time()
 
-    leads["created_at"] = pd.to_datetime(leads["created_at"], unit="s")
-    leads["updated_at"] = pd.to_datetime(leads["updated_at"], unit="s")
-    leads["closed_at"]  = pd.to_datetime(leads["closed_at"], unit="s")
+    leads["created_at"] = pd.to_datetime(leads["created_at"])
+    leads["updated_at"] = pd.to_datetime(leads["updated_at"])
+    leads["closed_at"]  = pd.to_datetime(leads["closed_at"])
 
     leads.to_gbq(
         "franchise_oddjob.dw_amocrm_fr_leads", if_exists="append"
@@ -31,9 +31,9 @@ def status_changes_sender():
 
     events = pd.read_csv("/home/analytics/OddJob/dags/temp_data/status_changes.csv")
 
-    events["created_at"] = pd.to_datetime(events["created_at"], unit="s")
-    events["updated_at"] = pd.to_datetime(events["updated_at"], unit="s")
-    events["closed_at"]  = pd.to_datetime(events["closed_at"], unit="s")
+    events["created_at"] = pd.to_datetime(events["created_at"])
+    events["updated_at"] = pd.to_datetime(events["updated_at"])
+    events["closed_at"]  = pd.to_datetime(events["closed_at"])
 
     client = bq.Client()
 
