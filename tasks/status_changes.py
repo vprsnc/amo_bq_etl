b91dd0adafd750563d99cd8a2a2702bb865e2dc8
Georgy Toporkov <83061636+vprsnc@users.noreply.github.com>
2022-08-08 19:02:15 +0300

    refactoring events to status_changes

---
+++ tasks/status_changes.py
@@ -0,0 +1,91 @@
+import os
+from datetime import timedelta, datetime
+from requests.adapters import HTTPAdapter
+
+from loguru import logger
+import pandas as pd
+
+from amocrm.v2 import tokens, filters
+from amocrm.v2 import Event
+from amocrm.v2.interaction import _session
+
+
+def get_events(now):
+     
+    client_id = os.getenv("AMO_CLIENT_ID_2")
+    client_secret = os.getenv("AMO_CLIENT_SECRET_2")
+    subdomain = os.getenv("AMO_SUBDOMAIN_2")
+    redirect_url = os.getenv("AMO_REDIRECT_URI_2")
+     
+    tokens.default_token_manager(
+        client_id=client_id,
+        client_secret=client_secret,
+        subdomain=subdomain,
+        redirect_url=redirect_url,
+        storage=tokens.FileTokensStorage(directory_path="/home/analytics/OddJob/tokens/events/")
+        )
+
+    code = os.getenv("CODE_2")
+
+    if code:
+         tokens.default_token_manager.init(code)
+         logger.debug("Access token has been used!")
+
+    _session.mount("https://", HTTPAdapter(max_retries=5))
+     
+    start = time.time()
+     
+    events = list(Event.objects.filter(
+        filters=[
+            filters.DateRangeFilter("created_at")(now-timedelta(days=1), now),
+            filters.SingleFilter("type")("lead_status_changed")
+            ]
+        ))
+     
+    end = time.time()
+     
+    logger.info(f"Downloaded {len(events)} in {end-start} seconds")
+     
+    events_dicts = []
+
+    for event in events:
+     
+        events_dicts.append(event.__dict__['_data'])
+     
+     
+    for event in events_dicts:
+     
+        event["id_status_before"] = event["value_before"][0]["lead_status"]["id"]
+        event["id_pipeline_before"] = event["value_before"][0]["lead_status"]["pipeline_id"]
+        event["id_status_after"] = event["value_after"][0]["lead_status"]["id"]
+        event["id_pipeline_after"] = event["value_after"][0]["lead_status"]["pipeline_id"]
+     
+    return events_dicts
+
+
+def parse_events(events):
+
+    df = pd.DataFrame(events)
+     
+    df.drop([
+        "value_after", "value_before", "_links", "_embedded"
+        ], axis=1, inplace=True)
+    df["created_at"] = pd.to_datetime(df["created_at"], unit="s")
+     
+    return df
+
+
+def store_events():
+
+    now = datetime.now()
+
+    events = get_events(now)
+
+    df = parse_events(events)
+
+    df.to_csv("/home/analytics/dags/temp_data/status_changes.csv", index=False)
+
+
+def cleanup_events():
+
+    os.remove("/home/analytics/dags/temp_data/status_changes.csv")
