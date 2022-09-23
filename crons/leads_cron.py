7ee5c6ae9f91283d2484df13aae78135126dd14c
Georgy <heavypresence@riesup.net>
2022-09-23 13:33:45 +0300

    renames corn to cron, fixed module path

--- crons/leads_cron.py
+++ crons/leads_cron.py
@@ -6,8 +6,8 @@ import datetime
 
 from loguru import logger
 
-from ..tasks.leads import store_leads
-from ..tasks.senders import leads_sender
+from tasks.leads import store_leads
+from tasks.senders import leads_sender
 
 
 def main():
