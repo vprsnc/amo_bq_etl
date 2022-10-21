650abb5ee565f8e818b1c740db41ca25315f686f
Georgy <heavypresence@riesup.net>
2022-10-21 19:02:06 +0300

    fixing tokens location

--- models.py
+++ models.py
@@ -5,18 +5,18 @@ from amocrm.v2 import Lead as _Lead
 from amocrm.v2 import tokens, custom_field
 
 
-client_id = os.getenv("AMO_CLIENT_ID")
-client_secret = os.getenv("AMO_CLIENT_SECRET")
-subdomain = os.getenv("AMO_SUBDOMAIN")
-redirect_url = os.getenv("AMO_REDIRECT_URI")
+# client_id = os.getenv("AMO_CLIENT_ID")
+# client_secret = os.getenv("AMO_CLIENT_SECRET")
+# subdomain = os.getenv("AMO_SUBDOMAIN")
+# redirect_url = os.getenv("AMO_REDIRECT_URI")
 
-tokens.default_token_manager(
-    client_id=client_id,
-    client_secret=client_secret,
-    subdomain=subdomain,
-    redirect_url=redirect_url,
-    storage=tokens.FileTokensStorage(directory_path="/home/analytics/OddJob/tokens/")
-    )
+# tokens.default_token_manager(
+#     client_id=client_id,
+#     client_secret=client_secret,
+#     subdomain=subdomain,
+#     redirect_url=redirect_url,
+#     storage=tokens.FileTokensStorage(directory_path="/home/analytics/tokens/")
+#     )
 
 # code = ...
 # if code:
