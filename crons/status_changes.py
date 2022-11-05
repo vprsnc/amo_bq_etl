61d479ab67b88bc1b536281ecbaeaeb75ed59f48
Georgy <heavypresence@riesup.net>
2022-11-05 16:34:29 +0300

    status changes

---
+++ crons/status_changes.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../tasks"))
+sys.path.append(os.path.abspath(".."))
+
+import datetime
+
+from loguru import logger
+
+from status_changes import store_events
+from senders import events_sender
+
+home = os.getenv("HOME")
+logger.add(f"{home}/logs/events.log", backtrace=True, rotation="500 kb")
+
+def main():
+    store_events()
+    now = datetime.datetime.now()
+    logger.info(f"Events downloaded successfully at {now}")
+
+    events_sender()
+    now = datetime.datetime.now()
+    logger.info(f"Events sent successfully at {now}")
+
+
+if __name__ == "__main__":
+    main()
