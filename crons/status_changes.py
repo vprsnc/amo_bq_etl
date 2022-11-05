1be4bd03375411c1e4b42dc412339be6fff6aac1
Georgy <heavypresence@riesup.net>
2022-11-05 16:39:38 +0300

    status changes

--- crons/status_changes.py
+++ crons/status_changes.py
@@ -7,15 +7,15 @@ import datetime
 
 from loguru import logger
 
-# from status_changes import store_events
-import status_changes
+from status_changes import store_events
+# import status_changes
 from senders import events_sender
 
 home = os.getenv("HOME")
 logger.add(f"{home}/logs/events.log", backtrace=True, rotation="500 kb")
 
 def main():
-    status_changes.store_events()
+    changes.store_events()
     now = datetime.datetime.now()
     logger.info(f"Events downloaded successfully at {now}")
 
