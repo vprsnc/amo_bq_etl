ca7d164cef2382e7312bb195ee555b1da4a32190
Georgy <heavypresence@riesup.net>
2022-12-04 16:53:41 +0300

    fixed if statement

--- cron_scripts/leads_franchise.sh
+++ cron_scripts/leads_franchise.sh
@@ -4,6 +4,6 @@ mydate=`date`
 cd $HOME/OddJob/crons
 $HOME/OddJob/venv/bin/python $HOME/OddJob/crons/leads_franchise.py
 if [ $? -eq 0 ]; then
-    echo "leads cron successful on $mydate" >> $HOME/test.cron
-else
+    echo "leads cron successful on $mydate" >> $HOME/test.cron; else
     echo "leads cron faield on $mydate" >> $HOME/test.cron
+fi
